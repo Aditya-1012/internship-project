d42cd83172c9d90348f12f0b123a965a23464555
Aditya-1012 <54428135+Aditya-1012@users.noreply.github.com>
2020-11-03 06:19:59 -0800

    Add files via upload

---
+++ joinroom.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using JetBrains.Annotations;
+using Photon.Realtime;
+
+public class joinroom : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    public GameObject opponentpanel = null;
+    public GameObject waitingpanel = null;
+    public Text waitingtext;
+    public bool isconnecting = false;
+    private const string gameversion = "0.2";
+    private const int maxplayers = 2;
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    // Update is called once per frame
+    public void findopponent()
+    {
+        isconnecting = true;
+        opponentpanel.SetActive(false);
+        waitingpanel.SetActive(true);
+        waitingtext.text = "searching";
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = gameversion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        if (isconnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        waitingpanel.SetActive(false);
+        opponentpanel.SetActive(false);
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxplayers });
+    }
+    public override void OnJoinedRoom()
+    {
+        int playercount = PhotonNetwork.CurrentRoom.PlayerCount;
+        if (playercount != maxplayers)
+        {
+            waitingtext.text = "waiting for opponent";
+        }
+        else
+        {
+            waitingtext.text = "opponent found";
+        }
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == maxplayers)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            waitingtext.text = "opponent found";
+           
+
+            PhotonNetwork.LoadLevel("level1");
+        }
+    }
+}
---
+++ uimanager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class uimanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField playerfield=null;
+    public Button enterbtn=null;
+    public const string playerprefskey = "playername";
+    public GameObject opponentpanel;
+    public GameObject instructionpanel;
+    void Start()
+    {
+        instructionpanel.SetActive(true);
+        enteringname();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerPrefs.DeleteAll();
+
+        }
+    }
+    public void enteringname()
+    {
+        if(!PlayerPrefs.HasKey(playerprefskey))
+        {
+            return;
+        }
+        
+        string defaultname = PlayerPrefs.GetString(playerprefskey);
+        playerfield.text = defaultname;
+        savingplayer(defaultname);
+    }
+    public void savingplayer(string name)
+    {
+        enterbtn.interactable = !string.IsNullOrEmpty(name);
+        
+    }
+    public void playersaved()
+    {
+        string playername = playerfield.text;
+        PhotonNetwork.NickName = playername;
+        PlayerPrefs.SetString(playerprefskey, playername);
+        opponentpanel.SetActive(true);
+        instructionpanel.SetActive(false);
+
+    }
+    public void onqiut()
+    {
+        Application.Quit();
+    }
+    public void onpressinstruction()
+    {
+        instructionpanel.SetActive(true);
+    }
+    public void onpressclose()
+    {
+        instructionpanel.SetActive(false);
+    }
+}
